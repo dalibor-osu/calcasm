d0b529767f430f2de7f42ae556210776748bc2e3
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2023-11-21 22:11:46 +0100

    rekt

--- calcasm.asm
+++ calcasm.asm
@@ -13,7 +13,14 @@ section .data
 	; Other messages
 	RESULT_MSG: db "Result: ", 0xa, 0
 	INPUT: times 100 db 0		; User input buffer
+	;INPUT: db "123", 0
 	LF: db 10
+	CONVERTED: db "Converted", 0xa, 0
+	const10: dd 10
+
+section .bss
+	e1_len resd 1
+	dummy resd 1
 
 section .text
 	global _start
@@ -28,20 +35,49 @@ _start:
 	sub rsp, 8			; reset stack pointer to free space
 
 	call read_user_int		; read user input as integer
-	push rax			; save it on stack
+	mov r10, rax			; save it on stack
 
 	push SECOND_NUMBER_PROMPT	; print_message(SECOND_NUMBER_PROMPT)
 	call print_message		; 
 	sub rsp, 8			; reset stack pointer to free space
 
 	call read_user_int		; read the second user input
-	push rax			; save it on stack
+	add r11, rax			; save it on stack
 
 	push OPERATOR_PROMPT		; print_message(OPERATOR_PROMPT)
 	call print_message		; 
 	sub rsp, 8			; reset stack pointer to free space
 
-	jmp _exit			; exit the program
+	call read_user_input
+	
+	cmp byte[INPUT], 0x2A
+	je _multiplication
+	cmp byte[INPUT], 0x2B
+	je _addition
+	cmp byte[INPUT], 0x2D
+	je _subtraction
+	cmp byte[INPUT], 0x2F
+	je _division
+
+_addition:
+	mov rax, r10
+	add rax, r11
+	jmp _print_result
+
+_subtraction:
+	mov rax, r10
+	sub rax, r11
+	jmp _print_result
+
+_multiplication:
+	mov ax, r10w
+	mul r11w
+	jmp _print_result
+
+_division:
+	mov ax, r10w
+	div r11w
+	jmp _print_result
 
 read_user_int:
 	push rbp
@@ -65,6 +101,28 @@ read_user_input:
 	mov rdx, 100
 	syscall
 
+	mov [e1_len], rax
+	cmp rax, rdx
+	jb .return
+	mov bl, [rsi + rax - 1]
+	cmp bl, 10
+	je .return
+	inc DWORD [e1_len]
+
+.loop:
+	mov rax, 0
+	mov rdi, 1
+	mov rsi, dummy
+	mov rdx, 1
+	syscall
+
+	test rax, rax
+	jz .return
+	mov al, [dummy]
+	cmp al, 10
+	jne .loop
+
+.return:
 	mov rsp, rbp
 	pop rbp
 	ret
@@ -93,7 +151,7 @@ calculate_string_length:
 	; Prologue
 	push rbp
 	mov rbp, rsp
-	
+
 	mov rdi, [rbp + 16]		; Move the string argument to rdi
 	mov rax, 0			; Set rax (ret value) to 0 
 
@@ -114,33 +172,55 @@ calculate_string_length:
 char_to_int:
 	push rbp
 	mov rbp, rsp
+	
+	xor rax, rax
+	xor rcx, rcx
+	mov rbx, INPUT
 
-	xor ax, ax
-	xor cx, cx
-	mov bx, 10
-	mov rsi, [rbp + 16]
-
-.convert_loop:
-	mov cl, [rsi]
-	cmp cl, byte 0
-	je .return
-
-	cmp cl, 0x30
-	jl _invalid_number
-	cmp cl, 0x39
-	jg _invalid_number
-
-	sub cl, 48
-	mul bx
-	add ax, cx
-	inc rsi
-
-	jmp .convert_loop
+.loop:
+	mov cl, [rbx]   ; cl = *rbx 
+        cmp rcx, 0      ; check for nul THIS IS WRONG
+        cmp rcx, 10     ; we have to check for NL ascii code 
+        je .return          ; if rcx == 0 goto end
+        imul rax, 10    ; rax *= 10
+        sub rcx, 48     ; rcx -= 48 (48 is acii for '0')
+        add rax, rcx    ; rax += rcx
+        inc rbx         ; rbx++
+        jmp .loop
 
 .return:
 	mov rsp, rbp
 	pop rbp
 	ret
+
+divide_by_ten:
+	div 10
+	ret
+
+print_number:
+	mov rcx, 10
+loop1:   
+	call divide_by_ten
+        add rax, 0x30
+        push rax
+        mov rax, rdx
+        dec rcx
+        jne loop1
+        mov rcx, 10        ;  digit count to print
+loop2:   
+	call print_message
+	pop rax
+        dec rcx
+        jne loop2
+	
+
+_print_result:
+	push RESULT_MSG
+	call print_message
+	add rbp, 8
+
+	call print_number
+	jmp _exit
 
 _invalid_number:
 	push INVALID_NUMBER_ERR
@@ -159,5 +239,5 @@ _exit_error:
 
 _exit:
 	mov rax, 60			; Set the exit syscall number
-	mov rdi, 0			; Set the return value to 0
+	mov rdi, r10			; Set the return value to 0
 	syscall				; Call the syscall
